556a9c993156b9cd001103dd3288e4bda81b2429
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-02 22:09:29 -0300

    add Component Card #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -8,15 +9,28 @@ import Fragmento from './components/basicos/Fragmento'
 export default () => (
 
     <div id="app">
-    <h1>Fundamentos React 4</h1>
-    <Aleatorio min={1} max={60} />
-        <Fragmento />
-        <ComParametro 
-            titulo="Situação d  o Aluno"
-            aluno="Pedro Silva" 
-            nota={9.3} />
 
-        <Primeiro></Primeiro>
+        <h1>Fundamentos React 4</h1>
+
+        <Card titulo="#4 - Desafio aleatório">
+            <Aleatorio min={1} max={60} />
+        </Card>
+
+        <Card titulo="#3- Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#2- Com pârametro">
+            <ComParametro
+                titulo="Situação do Aluno"
+                aluno="Pedro Silva"
+                nota={9.3} />
+        </Card>
+
+        <Card titulo="#01 - Primeiro componente">
+            <Primeiro></Primeiro>
+        </Card>
+
     </div>
 
 )
