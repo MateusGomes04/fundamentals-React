57bcdc9323ede1b2c81a31780d6e09c02b78308d
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-02 21:51:09 -0300

    add React fragment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './index.css'
 import ReactDOM from 'react-dom'
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
   <div id="app">
@@ -12,6 +13,8 @@ ReactDOM.render(
         titulo="Situação do Aluno"
         aluno="Pedro Silva" 
         nota={9.3} />
+
+  <Fragmento />
 </div>,
     document.getElementById('root')
     )
