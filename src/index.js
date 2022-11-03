d9199276a71887424b8ad5b25c2e5b3185a6f105
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-02 21:36:16 -0300

    add CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom' 
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const tag =  <strong>Olá React!</strong> 
 
 ReactDOM.render(
-    'Olá React !',
+  <div>
+  { tag }
+</div>,
     document.getElementById('root')
     )
